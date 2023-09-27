c0c0f02db4c84d16e1a0db803ed7326cfba70da1
Matheus Lopes <130188560+matheusfsanna@users.noreply.github.com>
2023-09-27 11:38:11 -0300

    Update tendencia.py

--- tendencia.py
+++ tendencia.py
@@ -1,8 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def angular(Y, X, n):  #coeficiente angular
+    a = (n*np.sum(X*Y)-np.sum(X)*np.sum(Y))/(n*np.sum(X**2)-np.sum(X)**2)
+    return a
+
+def linear (Y, X):  # coeficiente linear
+    b = np.mean(Y) - a*np.mean(X)
+    return b
+    
+
 url = 'https://raw.githubusercontent.com/alura-cursos/numpy/dados/apples_ts.csv'
 dado = np.loadtxt(url,delimiter=',',usecols=np.arange(1,88,1))
+
+
 
 
 dado_transposto = dado.T
@@ -27,9 +38,9 @@ Y = Moscow
 X = datas
 n = np.size(Moscow)
 
-a = (n*np.sum(X*Y)-np.sum(X)*np.sum(Y))/(n*np.sum(X**2)-np.sum(X)**2) #coeficiente angular
-b = np.mean(Y) - a*np.mean(X) # coeficiente linear
+a = angular(Y,X, n)
+b = linear(Y, X)
 y = a*X+b
 z = np.linalg.norm(Moscow-y) #correspondência
+print(z)
 plt.plot(X,y)
-
